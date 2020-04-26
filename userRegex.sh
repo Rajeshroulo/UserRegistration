1eff7fec0e140fc84045675c6f0e0048f5eb94ee
Rajeshroulo <rajraval017@gmail.com>
2020-04-26 13:00:08 +0530

    modified the code and uploaded

--- userRegex.sh
+++ userRegex.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -x
 
 namepat="^[A-Z][a-z]{3,}$"
-emailpat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+emailpat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$"
 phonepat="^[0-9]{2}[0-9]{10}$"
-passwordpat="^[0-9a-zA-Z]+[@#%&*][0-9a-zA-Z]{8,}$"
 
 read -p "Enter the phoneno:" number
 
@@ -14,7 +13,7 @@ else
     echo invalid
 fi
 
-function tocheckvalidation( ) {
+function patternvalidation( ) {
 if [[ $1 =~ $2 ]]
 then
     echo valid
@@ -24,12 +23,21 @@ fi
 }
 
 read -p "Enter the name:" name
-tocheckvalidation $name $namepat
+patternvalidation $name $namepat
 
 read -p "Enter the mail:" mail
-tocheckvalidation $mail $emailpat
+patternvalidation $mail $emailpat
+
+  UpperCase='^[0-9a-zA-Z][@$#!?&%]{1}[0-9a-zA-Z]{6,}$'
 
 read -p "Enter the password:" password
-tocheckvalidation $password $passwordpat
+
+      if [[ $password =~ $UpperCase ]]
+      then
+                echo Valid password
+      else
+                echo Invalid password
+
+       fi
 
 
